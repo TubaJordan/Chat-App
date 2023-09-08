af3429dc8398bba8b24cca53330623a1df6ea618
TubaJordan <113389465+TubaJordan@users.noreply.github.com>
2023-09-08 00:15:15 -0700

    inital setup

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Start from "./components/Start";
+import Screen2 from "./components/Chat";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen1">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Screen2" component={Screen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +31,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ components/Start.js
@@ -0,0 +1,107 @@
+import { StyleSheet, View, Text, Button, TextInput, ImageBackground, TouchableOpacity, ScrollView, KeyboardAvoidingView } from "react-native";
+import { useState } from "react";
+
+const image = require("../assets/Background-Image.png");
+
+const userBackgroundColors = {
+    a: "#090C08",
+    b: "#474056",
+    c: "#8A95A5",
+    d: "#B9C6AE",
+};
+
+
+const Start = ({ navigation }) => {
+
+    const [name, setName] = useState("");
+    const [color, setColor] = useState(userBackgroundColors.a);
+
+
+    return (
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <ScrollView contentContainerStyle={styles.container}>
+                <Text style={styles.appTitle}>Chat App</Text>
+                <View style={styles.inputContainer}>
+                    <KeyboardAvoidingView style={styles.inputContainer} behavior="padding" enabled>
+                        <TextInput style={styles.textInput} value={name} onChangeText={setName} placeholder="Your Name" placeholderTextColor="#757083" />
+                        <Text style={styles.textColorSelector}>Choose Background Color:</Text>
+                        <View style={styles.colorSelector}>
+                            <TouchableOpacity style={[styles.circle, color === userBackgroundColors.a && styles.selectedCircle, { backgroundColor: userBackgroundColors.a },]} onPress={() => setColor(userBackgroundColors.a)}></TouchableOpacity>
+                            <TouchableOpacity style={[styles.circle, color === userBackgroundColors.b && styles.selectedCircle, { backgroundColor: userBackgroundColors.b },]} onPress={() => setColor(userBackgroundColors.b)}></TouchableOpacity>
+                            <TouchableOpacity style={[styles.circle, color === userBackgroundColors.c && styles.selectedCircle, { backgroundColor: userBackgroundColors.c },]} onPress={() => setColor(userBackgroundColors.c)}></TouchableOpacity>
+                            <TouchableOpacity style={[styles.circle, color === userBackgroundColors.d && styles.selectedCircle, { backgroundColor: userBackgroundColors.d },]} onPress={() => setColor(userBackgroundColors.d)}></TouchableOpacity>
+                        </View>
+                    </KeyboardAvoidingView>
+                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Screen2", { name: name, color: color })}>
+                        <Text style={styles.buttonText}>Start Chatting</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'space-between',
+        padding: '6%',
+    },
+    appTitle: {
+        fontSize: 45,
+        fontWeight: '600',
+        color: '#FFFFFF',
+        alignSelf: 'center',
+        flex: 1,
+    },
+    inputContainer: {
+        backgroundColor: '#FFFFFF',
+        padding: '6%',
+        paddingBottom: 20,
+    },
+    textInput: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#757083',
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#757083',
+        marginTop: 15,
+        marginBottom: 15,
+    },
+    textColorSelector: {
+        fontSize: 16,
+        fontWeight: '300',
+        color: '#8A95A5',
+        marginBottom: 10,
+    },
+    colorSelector: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    circle: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+    },
+    selectedCircle: {
+        borderWidth: 2,
+        borderColor: '#FF0000',
+    },
+    button: {
+        backgroundColor: '#757083',
+        padding: 10,
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
+
+export default Start;
